15c5ddf5627d001f6befcbae716888aede37fbd4
Sebastian-080 <85071095+Sebastian-080@users.noreply.github.com>
2024-11-14 22:34:15 -0500

    Add files via upload

--- src/models/contacto.js
+++ src/models/contacto.js
@@ -1,4 +1,3 @@
-  
 class Contacto {
     constructor(id, nombre, correo, descripcion) {
         this.id = id;
@@ -9,3 +8,33 @@ class Contacto {
 }
 
 module.exports = Contacto;
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../util/db');
+
+const User = sequelize.define('contacto', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  correo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+},
+    {
+    tableName: 'contacto', // Especifica el nombre exacto de la tabla
+    timestamps: false   // Desactiva createdAt y updatedAt ya que son agregados automaticamente por el ORM
+  });
